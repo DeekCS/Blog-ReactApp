7e2856d5bd2d3b6bdab1e667c025c6660e452901
DeekCS <31720001079@std.bau.edu.jo>
2021-12-16 13:09:44 +0200

    convert from class to hooks

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,69 +1,28 @@
-import React, { Component } from "react";
+import React, {useEffect, useState} from "react";
 import { NavLink, Link } from "react-router-dom";
 import Navigation from "./Header.styled";
-// import { BurgerIcon } from './'
 
-class Nav extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isExpanded: false,
-            isLoggedIn: false,
-        };
-    }
-    handleToggle(e) {
-        e.preventDefault();
-        this.setState({
-            isExpanded: !this.state.isExpanded
-        });
+
+
+const Nav = () => {
+    const [isExpanded, setIsExpanded] = useState(true);
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const handleToggle = () => {
+        setIsExpanded(!isExpanded);
     }
 
-    //get user info from local storage if it exists
-    componentDidMount() {
+    const handleLogout = () => {
+        setIsLoggedIn(false);
+    }
+
+    useEffect(() => {
         const user = localStorage.getItem("user");
         if (user) {
-            this.setState({
-                isLoggedIn: true
-            });
+           setIsLoggedIn(true);
         }
-    }
-    //show link s if user is logged in
-
-    handleLogin =() =>{
-        // localStorage.getItem("users") ? this.setState({ isLoggedIn: false }) : this.setState({ isLoggedIn: true });
-
-        if (!(this.state.isLoggedIn)) {
-            return (
-            <NavLink activeClassName="active" to="/login">
-                <li>Logout</li>
-            </NavLink>
-            );
-        } else if (this.state.isLoggedIn === true) {
-            return (
-                <NavLink activeClassName="active" to="/register">
-                    <li>Login</li>
-                </NavLink>
-            );
-        }
-    }
-
-    //logout user
-
-    handleLogout(e) {
-        e.preventDefault();
-        localStorage.removeItem("users");
-        if(localStorage.getItem("users")){
-            this.setState({
-                isLoggedIn: false
-            });
-           this.history.push("/register");
-        }
-        }
-
-
-
-    render() {
-        const { isExpanded } = this.state;
+    }, [])
 
         return (
             <Navigation>
@@ -82,7 +41,7 @@ class Nav extends Component {
                     <i
                         className="fa fa-bars"
                         aria-hidden="true"
-                        onClick={e => this.handleToggle(e)}
+                        onClick={handleToggle}
                     />
                     <ul className={`collapsed ${isExpanded ? "is-expanded" : ""}`}>
                         <NavLink activeClassName="active" to="/">
@@ -91,12 +50,19 @@ class Nav extends Component {
                         <NavLink activeClassName="active" to="/post">
                             <li>BLOG</li>
                         </NavLink>
-                        {this.handleLogin()}
+                        {isLoggedIn ? (
+                            <NavLink onClick={handleLogout} activeClassName="active" to="/register">
+                                <li>Logout</li>
+                            </NavLink>
+                        ) : (
+                            <NavLink activeClassName="active" to="/login">
+                                <li>REGISTER</li>
+                            </NavLink>
+                        )}
                     </ul>
                 </nav>
             </Navigation>
         );
     }
-}
 
 export default Nav;
